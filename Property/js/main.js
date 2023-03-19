386e283bbbf4d9dd4ce41e95ceb0738d15467292
Anton Zelenov <84032262+zelka2000@users.noreply.github.com>
2023-03-19 12:47:39 +0300

    Add files via upload

---
+++ Property/js/main.js
@@ -0,0 +1,212 @@
+ScrollReveal().reveal('.logo-text', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.nav-link', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.button-header1', {
+    delay: 700,
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.button-header2', {
+    delay: 800,
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.button-header2', {
+    distance: '150px',
+    opacity: 0,
+    delay: 700,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.real-estate', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.title', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.subtitle', {
+    delay: 1000,
+    opacity: 0,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.header-buy-rent', {
+    delay: 1500,
+    opacity: 0,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.discover', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.title-recomendation', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.subtitle-recomendation', {
+    opacity: 0,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.btn-block-carousel', {
+    delay: 1000,
+    duration: 1000,
+    opacity: 0,
+    origin: 'bottom'
+});
+
+ScrollReveal().reveal('.carousel', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'bottom',
+    interval: 100
+});
+
+ScrollReveal().reveal('.our-advente', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.title-info', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.info', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left',
+    interval: 500
+});
+
+ScrollReveal().reveal('.img-block4', {
+    opacity: 0,
+    duration: 2500
+});
+
+ScrollReveal().reveal('.about-as', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.title-about', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.subtitle-about', {
+    delay: 500,
+    opacity: 0,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.btn-about', {
+    delay: 1000,
+    opacity: 0,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.title-block4', {
+    delay: 1500,
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'top'
+});
+
+ScrollReveal().reveal('.star-img', {
+    delay: 2500,
+    distance: '50px',
+    opacity: 0,
+    duration: 1000,
+    interval: 100,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.bar', {
+    delay: 1500,
+    distance: '250px',
+    opacity: 0,
+    duration: 1500,
+    origin: 'rigt'
+});
+
+ScrollReveal().reveal('.customer-termenal', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.title-customer-termenal', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.card-1', {
+    distance: '150px',
+    opacity: 0,
+    duration: 1000,
+    origin: 'left'
+});
+
+
+
+
+
+
+
+
+
+
