8c277559e990f17f596c60bf5b5088dd7a56b436
Anton Zelenov <84032262+zelka2000@users.noreply.github.com>
2023-05-19 17:19:04 +0300

    Delete Plan Web directory

--- Plan Web/go.js
+++
@@ -1,5 +0,0 @@
-up = document.querySelector(".up");
-up.addEventListener("click", function () {
-  document.body.scrollTop = 500; // For Safari
-  document.documentElement.scrollTop = 500; // For Chrome, Firefox, IE and Opera
-});
